68cd8bba78a45427d9b8b0549d8345283d7cc52c
Andrea Brioschi <brioschiandrea@tiscali.it>
2023-04-03 15:18:05 +0200

    base

---
+++ components/VerticalTable.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Badge, Descriptions } from 'antd';
+
+interface VerticalTableProps {
+    data: [{
+        key: string,
+        value: React.ReactNode,
+    }]
+}
+
+function VerticalTable({ props }: { props: VerticalTableProps }) {
+    return < Descriptions bordered>
+        {
+            props.data.map((item) =>
+                <Descriptions.Item span={4} label={item.key}>{item.value}</Descriptions.Item>)
+        }
+    </Descriptions>
+
+}
+
+export default VerticalTable;
